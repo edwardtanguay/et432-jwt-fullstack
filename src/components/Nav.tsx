cde5c0ffebc13fe44e210a88276607f4e5598673
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-15 15:36:06 +0100

    login/logout switch works

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,15 +3,33 @@ import { NavLink } from "react-router-dom";
 import { AppContext } from "../AppContext";
 
 export const Nav = () => {
-	const { handleLogout } = useContext(AppContext);
+	const { handleLogout, currentUser } = useContext(AppContext);
 
-	return <nav>
-		<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
-			<li><NavLink to="/welcome">Welcome</NavLink></li>
-			<li><NavLink to="/books">Books</NavLink></li>
-			<li><NavLink to="/users">Users</NavLink></li>
-			<li><NavLink to="/login">Login</NavLink></li>
-			<li><a className="cursor-pointer" onClick={handleLogout}>Logout</a></li>
-		</ul>
-	</nav>;
+	return (
+		<nav>
+			<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
+				<li>
+					<NavLink to="/welcome">Welcome</NavLink>
+				</li>
+				<li>
+					<NavLink to="/books">Books</NavLink>
+				</li>
+				<li>
+					<NavLink to="/users">Users</NavLink>
+				</li>
+				{currentUser.fullName.trim() === "" && (
+					<li>
+						<NavLink to="/login">Login</NavLink>
+					</li>
+				)}
+				{currentUser.fullName.trim() !== "" && (
+					<li>
+						<a className="cursor-pointer" onClick={handleLogout}>
+							Logout
+						</a>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
 };
